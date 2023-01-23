9d7bb82c1cdefa86feccad8462435ef922af1902
MxMhx <kantinan2016@gmail.com>
2023-01-23 16:00:38 +0700

    api

--- fastmeal/lib/services/api_service.dart
+++ fastmeal/lib/services/api_service.dart
@@ -6,11 +6,12 @@ import 'package:fastmeal/services/ordermodel.dart';
 class APIService {
   late List<OrderModel> _dataFromAPI;
 
-  Future<void> getOrder() async{
-    var url = Uri(scheme: 'https',host: '63cad10a4f53a004202b9d38.mockapi.io',path: '/myshop/orders/testorder');
+  Future<void> getOrder() async {
+    var url = Uri(
+        scheme: 'https',
+        host: '63cad10a4f53a004202b9d38.mockapi.io',
+        path: '/myshop/orders/testorder');
     var response = await http.get(url);
-    setState((){
-      _dataFromAPI = orderModelFromJson(response.body); //JSON to Dart object
-    });
+    _dataFromAPI = orderModelFromJson(response.body); //JSON to Dart object
   }
 }
--- fastmeal/lib/widgets/orderwidget.dart
+++ fastmeal/lib/widgets/orderwidget.dart
@@ -11,20 +11,6 @@ class OrderContainer extends StatefulWidget {
 }
 
 class _OrderContainerState extends State<OrderContainer> {
-  final List<OrderModel> ordermodel = [
-    OrderModel(
-        name: 'รรรรร',
-        menu: ['กะเพรา', 'แกงกะหรี่'],
-        amount: [1, 2],
-        note: ['ไข่ดาว', ''],
-        price: [50, 60]),
-    OrderModel(
-        name: 'จุ๊มเหม่ง มีอะไร',
-        menu: ['ข้าวผัด'],
-        amount: [1],
-        note: [''],
-        price: [60]),
-  ];
 
   final getorder = APIService();
 
@@ -36,148 +22,15 @@ class _OrderContainerState extends State<OrderContainer> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: ordermodel.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                    margin: const EdgeInsets.only(top: 20),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: light_orange,
-                    ),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              'ชื่อ ${ordermodel[index].name}',
-                              style: bold.copyWith(
-                                fontSize: 18,
-                                color: black,
-                              ),
-                            )
-                          ],
-                        ),
-                        //menuOrderList
-                        Container(
-                          margin: const EdgeInsets.symmetric(vertical: 10),
-                          child: Column(
-                            children: [
-                              ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: ordermodel[index].menu.length,
-                                itemBuilder: (context, index2) => Container(
-                                  margin: const EdgeInsets.symmetric(vertical: 5),
-                                  child: Column(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Text(
-                                            ordermodel[index].menu[index2],
-                                            style: light.copyWith(
-                                              fontSize: 16,
-                                              color: black,
-                                            ),
-                                          ),
-                                          const Spacer(),
-                                          Text(
-                                            'x${ordermodel[index].amount[index2]}',
-                                            style: light.copyWith(
-                                              fontSize: 16,
-                                              color: black,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        children: [
-                                          Container(
-                                            padding: EdgeInsets.symmetric(
-                                                horizontal: ordermodel[index]
-                                                            .note[index2] ==
-                                                        ''
-                                                    ? 0
-                                                    : 10),
-                                            decoration: BoxDecoration(
-                                                color: orange,
-                                                borderRadius:
-                                                    BorderRadius.circular(8)),
-                                            child: Text(
-                                              ordermodel[index].note[index2],
-                                              style: bold.copyWith(
-                                                fontSize: 16,
-                                                color: black,
-                                              ),
-                                            ),
-                                          ),
-                                          const Spacer(),
-                                          Text(
-                                            '${ordermodel[index].price[index2]}',
-                                            style: light.copyWith(
-                                              fontSize: 16,
-                                              color: black,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              'รวม : ',
-                              style: light.copyWith(
-                                fontSize: 16,
-                                color: black,
-                              ),
-                            ),
-                            const Spacer(),
-                            Text(
-                              '${ordermodel.map((e) => e.price[e.price.length-1] * e.amount[e.amount.length-1]).reduce((value, element) => value + element)}',
-                              style: light.copyWith(
-                                fontSize: 16,
-                                color: black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              )
-            ],
-          ),
-        ),
-        ButtonWidget(text: 'ปิดรับออเดอร์', textcolor: white, bordercolor: black, fieldcolor: black, textsize: 20, onTap: () {})
-      ],
+    return FutureBuilder(
+      future: getorder.getOrder(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          var result = snapshot.data;
+          return Column();
+        }
+        return CircularProgressIndicator();
+      },
     );
   }
-}
-
-class OrderModel {
-  final String name;
-  final List<String> menu, note;
-  final List<int> price, amount;
-
-  OrderModel(
-      {required this.name,
-      required this.menu,
-      required this.amount,
-      required this.note,
-      required this.price});
 }
